19f991cc0c69f0915c194fa1c7f622be6857f97c
taxi_driverr <xerxesxerxes13@gmail.com>
2026-06-04 07:14:33 +0530

    added comments

--- src/main/java/com/example/TodoAPISpring/TodoController.java
+++ src/main/java/com/example/TodoAPISpring/TodoController.java
@@ -1,12 +1,24 @@
 package com.example.TodoAPISpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+/*
+* as spring executes our code, annotations in spring are to indicate spring
+* which code to execute,
+* example : @GetMapping - if a get request comes execute this.
+* what is the use case of code,
+* example : @RestController - this class acts as a controller.
+* where to find data,
+* example : @RequestBody - the data comes in body of http request,
+* @PathVariable - the data comes as url param
+* what to do,
+* example - @ResponseBody - return the data in http response body.
+* */
 
 @RestController
 public class TodoController {
@@ -19,13 +31,27 @@ public class TodoController {
     }
 
     @GetMapping("/todos")
+    @ResponseStatus(HttpStatus.OK)
     public List<Todo> getTodoList(){
         return todoList;
     }
 
     @PostMapping("/todos")
+    @ResponseStatus(HttpStatus.CREATED)
     public Todo createTodoList(@RequestBody Todo newTodo){
         todoList.add(newTodo);
         return newTodo;
+    }
+/*
+instead of ResponseStatus annotation we can use ResponseEntity class to
+return the http status code along with data in the body.
+*/
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity<Todo> getTodoList(@PathVariable int todoId){
+        for(Todo toDo : todoList) {
+            if (toDo.getId() == todoId)
+                return ResponseEntity.status(HttpStatus.OK).body(toDo);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
